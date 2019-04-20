cc833fc57e073532491b09508527472b93806a6f
Never04eK <heket844@gmail.com>
2019-04-20 04:59:37 +0300

    good commit

---
+++ app/src/main/java/com/company/narodnyiapp/RegistrationActivity.java
@@ -0,0 +1,8 @@
+package com.company.narodnyiapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class RegistrationActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/company/narodnyiapp/RulesActivity.java
+++ app/src/main/java/com/company/narodnyiapp/RulesActivity.java
@@ -2,7 +2,6 @@ package com.company.narodnyiapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class RulesActivity extends AppCompatActivity {
