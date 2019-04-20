32dd79c93fb45fbd7e6a1a2d63f04f594fc1a943
Never04eK <heket844@gmail.com>
2019-04-20 04:47:40 +0300

    add registation

--- app/src/main/java/com/company/narodnyiapp/MainActivity.java
+++ app/src/main/java/com/company/narodnyiapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.company.narodnyiapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen);
+        setContentView(R.layout.activity_registration);
     }
 
     public void onClick(View view) {
         Intent myIntent = new Intent(this, RulesActivity.class);
         MainActivity.this.startActivity(myIntent);
     }
+
+
 }
