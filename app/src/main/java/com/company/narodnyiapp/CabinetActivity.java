e97d124bd29e96ad4a7f3008b5a8bc5a4409009e
Never04eK <heket844@gmail.com>
2019-04-22 15:43:56 +0300

    edit cabinet activity

--- app/src/main/java/com/company/narodnyiapp/CabinetActivity.java
+++ app/src/main/java/com/company/narodnyiapp/CabinetActivity.java
@@ -3,7 +3,7 @@ package com.company.narodnyiapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class cabinetActivity extends AppCompatActivity {
+public class CabinetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
