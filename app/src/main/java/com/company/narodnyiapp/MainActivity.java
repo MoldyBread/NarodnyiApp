307a6d50a836287800102262d5a651ef9c9e9895
Never04eK <heket844@gmail.com>
2019-04-22 13:04:04 +0300

    add camera action

--- app/src/main/java/com/company/narodnyiapp/MainActivity.java
+++ app/src/main/java/com/company/narodnyiapp/MainActivity.java
@@ -1,30 +1,30 @@
 package com.company.narodnyiapp;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageButton btn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= findViewById(R.id.cameraButton);
+        btn.setOnClickListener(v -> {
+            Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(i);
+        });
     }
 
     public void onClick(View view) {
         Intent myIntent = new Intent(this, RulesActivity.class);
         MainActivity.this.startActivity(myIntent);
-    }
-
-    public void cameraOnClick(View view) {
-        //TODO: Camera intent
     }
 
 
