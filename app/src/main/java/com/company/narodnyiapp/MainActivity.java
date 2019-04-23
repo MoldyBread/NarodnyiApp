424a7b6d16089cd441ba89f696b1a62661c0b1b1
Dan <lyash.dv@gmail.com>
2019-04-23 10:41:04 +0300

    MainActivity

--- app/src/main/java/com/company/narodnyiapp/MainActivity.java
+++ app/src/main/java/com/company/narodnyiapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.company.narodnyiapp;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -11,7 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
-
+import android.widget.TextView;
 
 
 import com.company.narodnyiapp.rules.RulesActivity;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton btn;
 
+
+    int CAMERA_R;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,8 +33,14 @@ public class MainActivity extends AppCompatActivity {
         btn= findViewById(R.id.cameraButton);
         btn.setOnClickListener(v -> {
             Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivity(i);
+            startActivityForResult(i,CAMERA_R);
+
         });
+
+        placeholder=findViewById(R.id.all_rq);
+
+        placeholder.setText(getIntent().getStringExtra("message"));
+
 
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
@@ -41,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, RulesActivity.class);
         MainActivity.this.startActivity(myIntent);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == -1) {
+
+            Intent i1=new Intent(this,SentForm.class);
+            startActivity(i1);
+        }
+    }
+
+    TextView placeholder;
+
+
 
 
 
--- app/src/main/java/com/company/narodnyiapp/SentForm.java
+++ app/src/main/java/com/company/narodnyiapp/SentForm.java
@@ -1,7 +1,12 @@
 package com.company.narodnyiapp;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SentForm extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class SentForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sent_form);
+
+
+    }
+
+    public void onClick(View view) {
+        Intent intent=new Intent(this, MainActivity.class);
+        TextView textView = findViewById(R.id.carPlate);
+        intent.putExtra("message",textView.getText());
+        startActivity(intent);
     }
 }
